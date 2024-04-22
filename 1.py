58b1d0ec168048d90ef3c07540e5118ce1a0f684
Daria <bezruchkodasha2107@gmail.com>
2024-04-22 16:25:22 +0300

    исправление

--- 1.py
+++ 1.py
@@ -4,9 +4,8 @@
 # value3 = input("Введите третье значение: ")
 
 # print(value1, value2, value3)  
-# print(value1, ":", value2, ":", value3)  
-# print("- " + value1, "\n- " + value2, "\n- " + value3) 
-
+# print(value1, value2, value3, sep = ':')  
+# print("- " + value1, value2, value3, sep="\n- ")
 #Exircise 2
 # value1 = int(input("Введите первое числа "))
 # value2 = int(input("Введите второе числа "))
@@ -27,14 +26,14 @@
 # print(f"Цена продажи со скидкой 15%: {discount_15}")
 
 #Exircise 4
-number = int(input("Введите число: "))
+# number = int(input("Введите число: "))
 
-number_of_thousands = number // 1000
-number_of_hundreds = (number % 1000) // 100
-number_of_tens = (number % 100) // 10
-number_of_units = number % 10
+# number_of_thousands = number // 1000
+# number_of_hundreds = (number % 1000) // 100
+# number_of_tens = (number % 100) // 10
+# number_of_units = number % 10
 
-print(f"тысяч - {number_of_thousands}")
-print(f"сотен - {number_of_hundreds}")
-print(f"десятков - {number_of_tens}")
-print(f"единиц - {number_of_units}")
+# print(f"тысяч - {number_of_thousands}")
+# print(f"сотен - {number_of_hundreds}")
+# print(f"десятков - {number_of_tens}")
+# print(f"единиц - {number_of_units}")
